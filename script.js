807de719c4bc185fff6f7c84b3799e2704f8462e
neslihanbaysal <neslihanbaysal2@gmail.com>
2024-09-30 15:03:16 +0300

    Slider ve ilk resme link eklendi

---
+++ script.js
@@ -0,0 +1,14 @@
+const postContainer = document.querySelector('.post-wrapper'); // Tüm postlar burada
+const posts = document.querySelectorAll('.post'); // Tüm postları al
+
+// Sağ oka tıklanınca (Next)
+document.getElementById('prevBtn').addEventListener('click', () => {
+    const firstPost = postContainer.firstElementChild; // İlk post'u al
+    postContainer.appendChild(firstPost); // İlk post'u sona taşı
+});
+
+// Sol oka tıklanınca (Previous)
+document.getElementById('nextBtn').addEventListener('click', () => {
+    const lastPost = postContainer.lastElementChild; // Son post'u al
+    postContainer.insertBefore(lastPost, postContainer.firstElementChild); // Son post'u başa taşı
+});
